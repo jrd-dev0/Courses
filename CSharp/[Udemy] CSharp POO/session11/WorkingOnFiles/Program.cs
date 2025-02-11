4f5ee69009b9cde1542a5791f7c8916342f11bab
JrD <198513937+jrd-dev0@users.noreply.github.com>
2025-02-10 21:40:48 -0300

    Resending some C# projects that encountered an error during commit
    Reenviando alguns projetos de C# que apresentaram erro no envio.

---
+++ CSharp/[Udemy] CSharp POO/session11/WorkingOnFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+string source = @"c:\windows\system32\drivers\etc\hosts";
+string destiny = @"c:\temp\FileSource.bkp";
+
+try 
+{   
+    // get source file and backup it on destiny folder
+    FileInfo file = new FileInfo(source);
+    file.CopyTo(destiny);
+
+    // get source file then show his content 
+    string[] hostFile = File.ReadAllLines(source);
+
+    foreach (string line in hostFile)
+    {
+        Console.WriteLine(line);
+    }
+
+}
+catch (IOException e )
+{
+    Console.WriteLine("Ocorreu um erro");
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadLine();
